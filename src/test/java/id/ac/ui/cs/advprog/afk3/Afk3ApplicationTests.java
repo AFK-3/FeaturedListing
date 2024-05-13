3d230cb40af4b7baa066e6b728cf72cf615fd781
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2024-05-14 01:04:42 +0700

    Fix test

--- src/test/java/id/ac/ui/cs/advprog/afk3/Afk3ApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/afk3/Afk3ApplicationTests.java
@@ -2,8 +2,10 @@ package id.ac.ui.cs.advprog.afk3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Afk3ApplicationTests {
 
 	@Test
