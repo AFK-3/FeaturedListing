902ffaf428bbf58bf4884a3452457f193f9488b9
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2024-05-26 19:06:56 +0700

    Edit listing middleware

--- src/main/java/id/ac/ui/cs/advprog/afk3/middleware/ListingMiddleware.java
+++ src/main/java/id/ac/ui/cs/advprog/afk3/middleware/ListingMiddleware.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.afk3.middleware;
 
 import id.ac.ui.cs.advprog.afk3.model.Listing;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,9 +11,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 public class ListingMiddleware {
-    public static final String sellUrl = "http://34.126.165.220/";
+    public static final String sellUrl = "http://35.198.243.155/";
     public static Listing getListing(String id, String token) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -27,6 +29,30 @@ public class ListingMiddleware {
                     HttpMethod.GET,
                     entity,
                     Listing.class
+            );
+            return response.getBody();
+        } catch (RestClientException e) {
+            System.err.println("Request failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static List<Listing> getAllListings(String token) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", token);
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        try {
+            URI uri = UriComponentsBuilder.fromUriString(sellUrl + "/listing/get-all")
+                    .build()
+                    .toUri();
+
+            ResponseEntity<List<Listing>> response = restTemplate.exchange(
+                    uri,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<Listing>>() {}
             );
             return response.getBody();
         } catch (RestClientException e) {
